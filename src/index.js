f009e79dfc00feeb2e51cf59e4c5b552346f1e19
Jinx66 <1577478705@qq.com>
2025-02-13 20:07:31 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 // src/index.js
 import JinxBadges from './views/badges.vue';
+import JinxIndex from './views/index.vue';
 
 JinxBadges.install = function(Vue) {
   Vue.component(JinxBadges.name, JinxBadges);
 };
 
-export default JinxBadges;
+JinxIndex.install = function(Vue) {
+  Vue.component(JinxIndex.name, JinxIndex);
+};
+
+export default {JinxBadges,JinxIndex};
